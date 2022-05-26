32decacfde43e8ea34fe1f1841b285e7cece9a7f
Takuma Otake <96374682+bigbamboo-jp@users.noreply.github.com>
2022-05-26 20:43:15 +0900

    The ssplit method was changed, the srsplit method was added, and the order of the arguments of each method was changed.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A Python module for manipulating parts of a string that are not surrounded by sp
 ## Features
 * Separate the part surrounded by a specific symbol and the part not surrounded by a character string.
 * Perform a string operation (replacement, etc.) on the part of the string that is not surrounded by a specific symbol.
-* Check if the string starts or ends with a specific character (s).
+* Checks if a string is surrounded by multiple types of strings (can be forward or backward only).
 ## How to use
 1. Import the sstr module
    
@@ -25,22 +25,22 @@ A Python module for manipulating parts of a string that are not surrounded by sp
    
    Example 1: Separate the parts surrounded and not surrounded by a specific symbol
    ```
-   divided_data = new_string.divide('"')  # (sep)
+   divided_data = new_string.divide(enclosure='"')  # (enclosure)
    print(divided_data)  # ['This is a ', '"test"', '.']
    print(type(divided_data[0]))  # <class 'str'>
    ```
    Example 2: Replace the characters in the part not surrounded by a specific symbol
    ```
-   edited_string = new_string.sreplace('"', 's', '#')  # (sep, __old, __new)
+   edited_string = new_string.sreplace('s', '#', enclosure='"')  # (__old, __new, enclosure)
    print(edited_string)  # Thi# i# a "test".
    print(type(edited_string))  # <class 'str'>
    ```
    Example 3: Count the number of "s" in the part not surrounded by a specific symbol (when the surrounding symbols are different before and after)
    ```
    new_string = sstr('This is a "test". This is also a (test).')
-   # quantity = new_string.scount('"', 's')  # (sep, x)
-   # quantity = new_string.scount([['(', ')']], 's')  # (sep, x)
-   quantity = new_string.scount(['"', ['(', ')']], 's')  # (sep, x)
+   # quantity = new_string.scount('s', enclosure='"')  # (x, enclosure)
+   # quantity = new_string.scount('s', enclosure=[['(', ')']])  # (x, enclosure)
+   quantity = new_string.scount('s', enclosure=['"', ['(', ')']])  # (x, enclosure)
    print(quantity)  # 5
    print(type(quantity))  # <class 'int'>
    ```
--- sstr.py
+++ sstr.py
@@ -1,205 +1,257 @@
 import re
+from typing import SupportsIndex
 
 
 class sstr(str):
     def __init__(self, string: str) -> None:
         self = string
 
-    def divide(self, sep, consider_escape: bool = False) -> list[str]:
+    def divide(self, enclosure=None, consider_escape: bool = False) -> list[str]:
         if consider_escape == True:
             template = "{0}.*?(?<!\\\\){1}"
         else:
             template = "{0}.*?{1}"
-        if type(sep) is not list and type(sep) is not tuple:
-            sep = [sep]
-        sep = [s for s in sep if s is not None]
-        if "" in sep:
-            raise ValueError("empty separator")
-        if sep == []:
+        if type(enclosure) is not list and type(enclosure) is not tuple:
+            enclosure = [enclosure]
+        enclosure = [e for e in enclosure if e is not None]
+        if "" in enclosure:
+            raise ValueError("empty enclosure")
+        if enclosure == []:
             return [str(self)]
         pattern = ""
-        for s in sep:
-            if type(s) is list or type(s) is tuple:
-                pattern += "|" + template.format(re.escape(s[0]), re.escape(s[1]))
+        for e in enclosure:
+            if type(e) is list or type(e) is tuple:
+                pattern += "|" + template.format(re.escape(e[0]), re.escape(e[1]))
             else:
-                s = re.escape(s)
-                pattern += "|" + template.format(s, s)
+                e = re.escape(e)
+                pattern += "|" + template.format(e, e)
         pattern = "(" + pattern.lstrip("|") + ")"
         divided_data = re.split(pattern, self)
         if divided_data[-1] == "" and len(divided_data) > 1:
             divided_data.pop(-1)
         return divided_data
 
-    def divide_and_classify(self, sep, consider_escape: bool = False) -> tuple[str, bool]:
-        divided_data = self.divide(sep=sep, consider_escape=consider_escape)
+    def divide_and_classify(self, enclosure=None, consider_escape: bool = False) -> tuple[str, bool]:
+        divided_data = self.divide(enclosure, consider_escape)
         for data in divided_data:
-            yield data, sstr.surroundedby(data, sep=sep)
+            yield data, sstr.surroundedby(data, enclosure)
 
-    def endswith_multiple(self, sep) -> bool:
+    def endswith_multiple(self, enclosure=None) -> bool:
         template = ".*{0}$"
-        if type(sep) is not list and type(sep) is not tuple:
-            sep = [sep]
-        sep = [s for s in sep if s is not None]
+        if type(enclosure) is not list and type(enclosure) is not tuple:
+            enclosure = [enclosure]
+        enclosure = [e for e in enclosure if e is not None]
+        if "" in enclosure:
+            raise ValueError("empty enclosure")
+        if enclosure == []:
+            return False
         pattern = ""
-        for s in sep:
-            if type(s) is list or type(s) is tuple:
-                pattern += "|" + template.format(re.escape(s[1]))
+        for e in enclosure:
+            if type(e) is list or type(e) is tuple:
+                pattern += "|" + template.format(re.escape(e[1]))
             else:
-                pattern += "|" + template.format(re.escape(s))
+                pattern += "|" + template.format(re.escape(e))
         pattern = "(" + pattern.lstrip("|") + ")"
         return re.fullmatch(pattern, self) != None
 
-    def get_inner_parts(self, sep, consider_escape: bool = False) -> list[str]:
-        divided_data = self.divide(sep=sep, consider_escape=consider_escape)
+    def get_inner_parts(self, enclosure=None, consider_escape: bool = False) -> list[str]:
+        divided_data = self.divide(enclosure, consider_escape)
         for data in divided_data:
-            if sstr.surroundedby(data, sep=sep) == False:
+            if sstr.surroundedby(data, enclosure) == False:
                 divided_data.remove(data)
         return divided_data
 
-    def get_outer_parts(self, sep, consider_escape: bool = False) -> list[str]:
-        divided_data = self.divide(sep=sep, consider_escape=consider_escape)
+    def get_outer_parts(self, enclosure=None, consider_escape: bool = False) -> list[str]:
+        divided_data = self.divide(enclosure, consider_escape)
         for data in divided_data:
-            if sstr.surroundedby(data, sep=sep) == True:
+            if sstr.surroundedby(data, enclosure) == True:
                 divided_data.remove(data)
         return divided_data
 
-    def scount(self, sep, *args, consider_escape: bool = False) -> int:
-        divided_data = self.divide(sep=sep, consider_escape=consider_escape)
+    def scount(self, *args, enclosure=None, consider_escape: bool = False, **kwargs) -> int:
+        divided_data = self.divide(enclosure, consider_escape)
         total_count = 0
         for i in range(len(divided_data)):
-            if sstr.surroundedby(divided_data[i], sep=sep) == False:
-                total_count += divided_data[i].count(*args)
+            if sstr.surroundedby(divided_data[i], enclosure) == False:
+                total_count += divided_data[i].count(*args, **kwargs)
         return total_count
 
-    def sfind(self, sep, *args, consider_escape: bool = False) -> int:
-        divided_data = self.divide(sep=sep, consider_escape=consider_escape)
+    def sfind(self, *args, enclosure=None, consider_escape: bool = False, **kwargs) -> int:
+        divided_data = self.divide(enclosure, consider_escape)
         checked_length = 0
         for i in range(len(divided_data)):
-            if sstr.surroundedby(divided_data[i], sep=sep) == False:
-                result = divided_data[i].find(*args)
+            if sstr.surroundedby(divided_data[i], enclosure) == True:
+                checked_length += len(divided_data[i])
+            else:
+                result = divided_data[i].find(*args, **kwargs)
                 if result == -1:
                     checked_length += len(divided_data[i])
                 else:
                     return checked_length + result
-            else:
-                checked_length += len(divided_data[i])
         return -1
 
-    def sindex(self, sep, *args, consider_escape: bool = False) -> int:
-        divided_data = self.divide(sep=sep, consider_escape=consider_escape)
+    def sindex(self, *args, enclosure=None, consider_escape: bool = False, **kwargs) -> int:
+        divided_data = self.divide(enclosure, consider_escape)
         checked_length = 0
         for i in range(len(divided_data)):
-            if sstr.surroundedby(divided_data[i], sep=sep) == False:
-                result = divided_data[i].find(*args)
+            if sstr.surroundedby(divided_data[i], enclosure) == True:
+                checked_length += len(divided_data[i])
+            else:
+                result = divided_data[i].find(*args, **kwargs)
                 if result == -1:
                     checked_length += len(divided_data[i])
                 else:
                     return checked_length + result
-            else:
-                checked_length += len(divided_data[i])
         raise ValueError("substring not found")
 
-    def slower(self, sep, *args, consider_escape: bool = False) -> str:
-        divided_data = self.divide(sep=sep, consider_escape=consider_escape)
+    def slower(self, *args, enclosure=None, consider_escape: bool = False, **kwargs) -> str:
+        divided_data = self.divide(enclosure, consider_escape)
         for i in range(len(divided_data)):
-            if sstr.surroundedby(divided_data[i], sep=sep) == False:
-                divided_data[i] = divided_data[i].lower(*args)
+            if sstr.surroundedby(divided_data[i], enclosure) == False:
+                divided_data[i] = divided_data[i].lower(*args, **kwargs)
         return "".join(divided_data)
 
-    def sreplace(self, sep, *args, consider_escape: bool = False) -> str:
-        divided_data = self.divide(sep=sep, consider_escape=consider_escape)
+    def sreplace(self, *args, enclosure=None, consider_escape: bool = False, **kwargs) -> str:
+        divided_data = self.divide(enclosure, consider_escape)
         for i in range(len(divided_data)):
-            if sstr.surroundedby(divided_data[i], sep=sep) == False:
-                divided_data[i] = divided_data[i].replace(*args)
+            if sstr.surroundedby(divided_data[i], enclosure) == False:
+                divided_data[i] = divided_data[i].replace(*args, **kwargs)
         return "".join(divided_data)
 
-    def srfind(self, sep, *args, consider_escape: bool = False) -> int:
-        divided_data = self.divide(sep=sep, consider_escape=consider_escape)
+    def srfind(self, *args, enclosure=None, consider_escape: bool = False, **kwargs) -> int:
+        divided_data = self.divide(enclosure, consider_escape)
         unchecked_length = len(self)
-        for i in reversed(range(len(divided_data))):
+        for i in list(reversed(range(len(divided_data)))):
             unchecked_length -= len(divided_data[i])
-            if sstr.surroundedby(divided_data[i], sep=sep) == False:
-                result = divided_data[i].rfind(*args)
+            if sstr.surroundedby(divided_data[i], enclosure) == False:
+                result = divided_data[i].rfind(*args, **kwargs)
                 if result != -1:
                     return unchecked_length + result
         return -1
 
-    def srindex(self, sep, *args, consider_escape: bool = False) -> int:
-        divided_data = self.divide(sep=sep, consider_escape=consider_escape)
+    def srindex(self, *args, enclosure=None, consider_escape: bool = False, **kwargs) -> int:
+        divided_data = self.divide(enclosure, consider_escape)
         unchecked_length = len(self)
-        for i in reversed(range(len(divided_data))):
+        for i in list(reversed(range(len(divided_data)))):
             unchecked_length -= len(divided_data[i])
-            if sstr.surroundedby(divided_data[i], sep=sep) == False:
-                result = divided_data[i].rfind(*args)
+            if sstr.surroundedby(divided_data[i], enclosure) == False:
+                result = divided_data[i].rfind(*args, **kwargs)
                 if result != -1:
                     return unchecked_length + result
         raise ValueError("substring not found")
 
-    def ssplit(self, _sep, *args, consider_escape: bool = False) -> list[str]:
-        divided_data = self.divide(sep=_sep, consider_escape=consider_escape)
+    def srsplit(self, *args, enclosure=None, consider_escape: bool = False, maxsplit: SupportsIndex = -1, **kwargs) -> list[str]:
+        divided_data = list(reversed(self.divide(enclosure=enclosure, consider_escape=consider_escape)))
         splitted_data = []
         for i in range(len(divided_data)):
-            if sstr.surroundedby(divided_data[i], sep=_sep) == True:
-                if len(splitted_data) == 0:
-                    splitted_data.append(divided_data[i])
+            if maxsplit != -1:
+                if len(splitted_data) == maxsplit + 1:
+                    splitted_data[-1] = "".join(list(reversed(divided_data[i:]))) + splitted_data[-1]
+                    break
+            if sstr.surroundedby(divided_data[i], enclosure=enclosure) == True:
+                if len(splitted_data) > 0:
+                    splitted_data[-1] = divided_data[i] + splitted_data[-1]
                 else:
-                    splitted_data[-1] += divided_data[i]
+                    splitted_data.append(divided_data[i])
             else:
-                partial_splitted_data = divided_data[i].split(*args)
-                if len(splitted_data) > 0 and len(partial_splitted_data) > 0:
-                    if sstr.endswith_multiple(splitted_data[-1], sep=_sep) == True:
-                        splitted_data[-1] += partial_splitted_data.pop(0)
-                splitted_data.extend(partial_splitted_data)
+                if maxsplit == -1:
+                    maxsplit_argument = -1
+                else:
+                    maxsplit_argument = maxsplit + 1 - len(splitted_data)
+                partial_splitted_data = list(reversed(divided_data[i].rsplit(maxsplit=maxsplit_argument, *args, **kwargs)))
+                if len(splitted_data) > 0:
+                    splitted_data[-1] = partial_splitted_data.pop(0) + splitted_data[-1]
+                else:
+                    if maxsplit != -1:
+                        if len(partial_splitted_data) > maxsplit_argument:
+                            partial_splitted_data[-2] = partial_splitted_data[-1] + kwargs["enclosure"] + partial_splitted_data[-2]
+                            partial_splitted_data.pop(-1)
+                splitted_data = list(reversed(partial_splitted_data)) + splitted_data
         return splitted_data
 
-    def sswapcase(self, sep, *args, consider_escape: bool = False) -> str:
-        divided_data = self.divide(sep=sep, consider_escape=consider_escape)
+    def ssplit(self, *args, enclosure=None, consider_escape: bool = False, maxsplit: SupportsIndex = -1, **kwargs) -> list[str]:
+        divided_data = self.divide(enclosure=enclosure, consider_escape=consider_escape)
+        splitted_data = []
         for i in range(len(divided_data)):
-            if sstr.surroundedby(divided_data[i], sep=sep) == False:
-                divided_data[i] = divided_data[i].swapcase(*args)
+            if maxsplit != -1:
+                if len(splitted_data) == maxsplit + 1:
+                    splitted_data[-1] = splitted_data[-1] + "".join(divided_data[i:])
+                    break
+            if sstr.surroundedby(divided_data[i], enclosure=enclosure) == True:
+                if len(splitted_data) > 0:
+                    splitted_data[-1] = splitted_data[-1] + divided_data[i]
+                else:
+                    splitted_data.append(divided_data[i])
+            else:
+                if maxsplit == -1:
+                    maxsplit_argument = -1
+                else:
+                    maxsplit_argument = maxsplit + 1 - len(splitted_data)
+                partial_splitted_data = divided_data[i].split(maxsplit=maxsplit_argument, *args, **kwargs)
+                if len(splitted_data) > 0:
+                    splitted_data[-1] = splitted_data[-1] + partial_splitted_data.pop(0)
+                else:
+                    if maxsplit != -1:
+                        if len(partial_splitted_data) > maxsplit_argument:
+                            partial_splitted_data[-2] = partial_splitted_data[-2] + kwargs["enclosure"] + partial_splitted_data[-1]
+                            partial_splitted_data.pop(-1)
+                splitted_data = splitted_data + partial_splitted_data
+        return splitted_data
+
+    def sswapcase(self, *args, enclosure=None, consider_escape: bool = False, **kwargs) -> str:
+        divided_data = self.divide(enclosure, consider_escape)
+        for i in range(len(divided_data)):
+            if sstr.surroundedby(divided_data[i], enclosure) == False:
+                divided_data[i] = divided_data[i].swapcase(*args, **kwargs)
         return "".join(divided_data)
 
-    def startswith_multiple(self, sep) -> bool:
+    def startswith_multiple(self, enclosure=None) -> bool:
         template = "^{0}.*"
-        if type(sep) is not list and type(sep) is not tuple:
-            sep = [sep]
-        sep = [p for p in sep if p is not None]
+        if type(enclosure) is not list and type(enclosure) is not tuple:
+            enclosure = [enclosure]
+        enclosure = [e for e in enclosure if e is not None]
+        if "" in enclosure:
+            raise ValueError("empty enclosure")
+        if enclosure == []:
+            return False
         pattern = ""
-        for p in sep:
-            if type(p) is list or type(p) is tuple:
-                pattern += "|" + template.format(re.escape(p[0]))
+        for e in enclosure:
+            if type(e) is list or type(e) is tuple:
+                pattern += "|" + template.format(re.escape(e[0]))
             else:
-                pattern += "|" + template.format(re.escape(p))
+                pattern += "|" + template.format(re.escape(e))
         pattern = "(" + pattern.lstrip("|") + ")"
         return re.fullmatch(pattern, self) != None
 
-    def stitle(self, sep, *args, consider_escape: bool = False) -> str:
-        divided_data = self.divide(sep=sep, consider_escape=consider_escape)
+    def stitle(self, *args, enclosure=None, consider_escape: bool = False, **kwargs) -> str:
+        divided_data = self.divide(enclosure, consider_escape)
         for i in range(len(divided_data)):
-            if sstr.surroundedby(divided_data[i], sep=sep) == False:
-                divided_data[i] = divided_data[i].title(*args)
+            if sstr.surroundedby(divided_data[i], enclosure) == False:
+                divided_data[i] = divided_data[i].title(*args, **kwargs)
         return "".join(divided_data)
 
-    def supper(self, sep, *args, consider_escape: bool = False) -> str:
-        divided_data = self.divide(sep=sep, consider_escape=consider_escape)
+    def supper(self, *args, enclosure=None, consider_escape: bool = False, **kwargs) -> str:
+        divided_data = self.divide(enclosure, consider_escape)
         for i in range(len(divided_data)):
-            if sstr.surroundedby(divided_data[i], sep=sep) == False:
-                divided_data[i] = divided_data[i].upper(*args)
+            if sstr.surroundedby(divided_data[i], enclosure) == False:
+                divided_data[i] = divided_data[i].upper(*args, **kwargs)
         return "".join(divided_data)
 
-    def surroundedby(self, sep) -> bool:
+    def surroundedby(self, enclosure=None) -> bool:
         template = "{0}.*{1}"
-        if type(sep) is not list and type(sep) is not tuple:
-            sep = [sep]
-        sep = [s for s in sep if s is not None]
-        if "" in sep:
-            raise ValueError("empty separator")
+        if type(enclosure) is not list and type(enclosure) is not tuple:
+            enclosure = [enclosure]
+        enclosure = [e for e in enclosure if e is not None]
+        if "" in enclosure:
+            raise ValueError("empty enclosure")
+        if enclosure == []:
+            return False
         pattern = ""
-        for s in sep:
-            if type(s) is list or type(s) is tuple:
-                pattern += "|" + template.format(re.escape(s[0]), re.escape(s[1]))
+        for e in enclosure:
+            if type(e) is list or type(e) is tuple:
+                pattern += "|" + template.format(re.escape(e[0]), re.escape(e[1]))
             else:
-                s = re.escape(s)
-                pattern += "|" + template.format(s, s)
+                e = re.escape(e)
+                pattern += "|" + template.format(e, e)
         pattern = "(" + pattern.lstrip("|") + ")"
         return re.fullmatch(pattern, self) != None
